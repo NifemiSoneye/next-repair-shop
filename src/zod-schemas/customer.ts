f7e82fda61155999c348b5c3417a101db535e0f9
NifemiSoneye <nifemisoneye@gmail.com>
2026-04-22 04:37:26 -0700

    Initialized zod schemas

---
+++ src/zod-schemas/customer.ts
@@ -0,0 +1,28 @@
+import { createInsertSchema, createSelectSchema } from "drizzle-zod";
+import { customers } from "@/db/schema";
+import z from "zod";
+
+export const insertCustomerSchema = createInsertSchema(customers, {
+  firstName: (schema) => schema.firstName.min(1, "First Name is required"),
+  lastName: (schema) => schema.lastName.min(1, "First Name is required"),
+  address1: (schema) => schema.address1.min(1, "Addressis required"),
+  city: (schema) => schema.city.min(1, "City is required"),
+  state: (schema) =>
+    schema.state.length(2, "State must be exactly 2 characters"),
+  email: (schema) => schema.email.email("Invalid email address"),
+  zip: (schema) =>
+    schema.zip.regex(
+      /^\d{5}(-\d{4})?$/,
+      "Invalid Zip code . Use 5 digits or 5 digits followed by a yphen and 4 digits",
+    ),
+  phone: (schema) =>
+    schema.phone.regex(
+      /^\d{3}-\d{3}-\d{4}$/,
+      "Invalid phone Number format . Use XXX-XXX-XXXX",
+    ),
+});
+
+export const selectCustomerSchema = createSelectSchema(customers);
+
+export type insertCustomerSchemaType = z.infer<typeof insertCustomerSchema>;
+export type selectCustomerSchemaType = z.infer<typeof selectCustomerSchema>;

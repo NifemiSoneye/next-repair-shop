f2eff0b7b006abdd6830be7f42f0e8a7178d7c79
NifemiSoneye <nifemisoneye@gmail.com>
2026-04-29 06:09:57 -0700

    Added table to customer search

--- src/app/(rs)/customers/page.tsx
+++ src/app/(rs)/customers/page.tsx
@@ -1,7 +1,8 @@
 import { get } from "http";
 import CustomerSearch from "./CustomerSearch";
 import { getCustomerSearchResults } from "@/lib/queries/getCustomerSearchResults";
-
+import * as Sentry from "@sentry/nextjs";
+import CustomerTable from "@/app/(rs)/customers/CustomerTable";
 export const metadata = {
   title: "Customer search",
 };
@@ -14,12 +15,21 @@ export default async function Customers({
 
   if (!searchText) return <CustomerSearch />;
 
+  const span = Sentry.startInactiveSpan({
+    name: "getCustomerSearchResults-2",
+  });
+
   const results = await getCustomerSearchResults(searchText);
 
+  span.end();
   return (
     <>
       <CustomerSearch />
-      <p>{JSON.stringify(results)}</p>
+      {results.length ? (
+        <CustomerTable data={results} />
+      ) : (
+        <p className="mt-4">No results found</p>
+      )}
     </>
   );
 
